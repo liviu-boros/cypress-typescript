0fe7ebff958968dc5e98a44e529c259810330e8e
liviu-boros <boros.liviu@gmail.com>
2023-03-20 18:50:48 +0200

    Navigation tests
    - updated Header.ts component class
    - updated locators for Navbar
    - added Navigation.cy.ts tests

---
+++ cypress/components/Header.ts
@@ -0,0 +1,49 @@
+import { Locators } from "../support/Locators"
+
+class Header {
+  public headerLogo(): Cypress.Chainable {
+    return cy.get(Locators.Header.brand)
+  }
+
+  public home(): Cypress.Chainable {
+    return cy.get(Locators.Header.home)
+  }
+
+  public categories(): Cypress.Chainable {
+    return cy.get(Locators.Header.categories)
+  }
+
+  public contact(): Cypress.Chainable {
+    return cy.get(Locators.Header.contact)
+  }
+
+  public handTools(): Cypress.Chainable {
+    return cy.get(Locators.Header.handTools)
+  }
+
+  public powerTools(): Cypress.Chainable {
+    return cy.get(Locators.Header.powerTools)
+  }
+
+  public specialTools(): Cypress.Chainable {
+    return cy.get(Locators.Header.specialTools)
+  }
+
+  public rentals(): Cypress.Chainable {
+    return cy.get(Locators.Header.rentals)
+  }
+
+  public singIn(): Cypress.Chainable {
+    return cy.get(Locators.Header.signIn)
+  }
+
+  public cart(): Cypress.Chainable {
+    return cy.get(Locators.Header.cart)
+  }
+
+  public cartQuantity(): Cypress.Chainable {
+    return cy.get(Locators.Header.cartQuantity)
+  }
+}
+
+export default new Header()
---
+++ cypress/support/Locators.ts
@@ -0,0 +1,15 @@
+export const Locators = {
+  Header: {
+    brand: ".navbar-brand",
+    home: '[data-test="nav-home"]',
+    categories: '[data-test="nav-categories"]',
+    handTools: '[data-test="nav-hand-tools"]',
+    powerTools: '[data-test="nav-power-tools"]',
+    specialTools: '[data-test="nav-power-tools"]',
+    rentals: '[data-test="nav-rentals"]',
+    contact: '[data-test="nav-contact"]',
+    signIn: '[data-test="nav-sign-in"]',
+    cart: '[data-test="nav-cart"]',
+    cartQuantity: '[data-test="cart-quantity"]',
+  },
+}
