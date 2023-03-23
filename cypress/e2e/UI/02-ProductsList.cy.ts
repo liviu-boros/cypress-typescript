89abe1d131c0bcf68a358ff0d6e94ea164179663
liviu-boros <boros.liviu@gmail.com>
2023-03-23 20:28:26 +0200

    Update 02-ProductsList.cy.ts

--- cypress/e2e/UI/02-ProductsList.cy.ts
+++ cypress/e2e/UI/02-ProductsList.cy.ts
@@ -1,8 +1,9 @@
-import { Data } from "../../support/Data"
-import ProductsGrid from "../../components/ProductsGrid"
-import { selectRandom, paginateProducts } from "../../support/Utils"
+import { Data } from "@data"
+import ProductsGrid from "@components/ProductsGrid"
+import { selectRandom, paginateProducts } from "@utilies"
 
 describe("Products", () => {
+  // visit home page
   beforeEach(() => {
     cy.visit("/")
   })
@@ -13,16 +14,17 @@ describe("Products", () => {
     const randomProduct = selectRandom(firstPageProducts)
     const { id, image, name, price } = randomProduct
 
+    // verify product card
+    // checking its id, image, name and price
+    // ! abstracted to ProductsGrid.verifyProduct
     ProductsGrid.productCard(id)
       .productImage()
       .should("be.visible")
       .should("have.attr", "src", `assets/img/products/${image}`)
-
     ProductsGrid.productCard(id)
       .productName()
       .should("be.visible")
       .should("contain.text", name)
-
     ProductsGrid.productCard(id)
       .productPrice()
       .should("be.visible")
@@ -35,14 +37,14 @@ describe("Products", () => {
     const secondPageProducts = paginateProducts(Data.Products, 2)
     const thirdPageProducts = paginateProducts(Data.Products, 3)
 
-    //
+    // verify products grid
     // first page products
     for (let product of firstPageProducts) {
       const { id, image, name, price } = product
       ProductsGrid.verifyProductFacade(id, image, name, price)
     }
 
-    //
+    // verify products grid
     // second page products
     ProductsGrid.nextPage().click()
     for (let product of secondPageProducts) {
@@ -50,7 +52,7 @@ describe("Products", () => {
       ProductsGrid.verifyProductFacade(id, image, name, price)
     }
 
-    //
+    // verify products grid
     // third page products
     ProductsGrid.nextPage().click()
     for (let product of thirdPageProducts) {
