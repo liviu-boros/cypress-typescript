9ca6d4255db413c1b7da2eff9bea15daaa81aa74
liviu-boros <boros.liviu@gmail.com>
2023-03-24 20:14:12 +0200

    Update HelperCart.ts
    - added jsdocs for methods

--- cypress/support/HelperCart.ts
+++ cypress/support/HelperCart.ts
@@ -6,6 +6,9 @@ interface Product {
   quantity?: number
 }
 
+/**
+ * Represents a HelperCart object that manages the list of products in the cart.
+ */
 export class HelperCart {
   private _cartList: Product[]
 
@@ -13,6 +16,10 @@ export class HelperCart {
     this._cartList = []
   }
 
+  /**
+   * Adds a new product to the cart or increases the quantity of an existing product.
+   * @param {Product} product - The product to add to the cart.
+   */
   addProduct(product: Product): void {
     const existingProduct = this._cartList.find(
       (el) => el.name === product.name
@@ -26,6 +33,11 @@ export class HelperCart {
     }
   }
 
+  /**
+   * Removes a product from the cart.
+   * @param {Product} product - The product to remove from the cart.
+   * @throws {Error} If the product is not found in the cart.
+   */
   removeProduct(product: Product): void {
     const existingProductIndex = this._cartList.findIndex(
       (el) => el.name === product.name
@@ -36,6 +48,12 @@ export class HelperCart {
     this._cartList.splice(existingProductIndex, 1)
   }
 
+  /**
+   * Increases the quantity of a product in the cart by a specified amount.
+   * @param {Product} product - The product to increase the quantity of.
+   * @param {number} times - The amount to increase the quantity by.
+   * @throws {Error} If the product is not found in the cart or its quantity is undefined.
+   */
   increaseQuantity(product: Product, times: number): void {
     const existingProduct = this._cartList.find(
       (el) => el.name === product.name
@@ -48,6 +66,12 @@ export class HelperCart {
     existingProduct.quantity += times
   }
 
+  /**
+   * Decreases the quantity of a product in the cart by a specified amount.
+   * @param {Product} product - The product to decrease the quantity of.
+   * @param {number} times - The amount to decrease the quantity by.
+   * @throws {Error} If the product is not found in the cart or its quantity is undefined.
+   */
   decreaseQuantity(product: Product, times: number): void {
     const existingProduct = this._cartList.find(
       (el) => el.name === product.name
@@ -60,11 +84,22 @@ export class HelperCart {
     existingProduct.quantity -= times
   }
 
+  /**
+   * Returns the list of products in the cart.
+   * @returns {Product[]} The list of products in the cart.
+   * @throws {Error} If the cart is empty.
+   */
   getCart(): Product[] {
     if (this._cartList.length === 0) throw new Error("Cart is empty")
     return this._cartList
   }
 
+  /**
+   * Returns the total price of a specific product in the cart.
+   * @param {Product} product - The product to get the total price of.
+   * @returns {string} The total price of the product, formatted to 2 decimal places.
+   * @throws {Error} If the product is not found in the cart or its quantity is undefined.
+   */
   getTotalProductPrice(product: Product): string {
     const existingProduct = this._cartList.find(
       (el) => el.name === product.name
@@ -77,6 +112,12 @@ export class HelperCart {
     return (existingProduct.price * existingProduct.quantity).toFixed(2)
   }
 
+  /**
+   * Returns the quantity of a specific product in the cart.
+   * @param {Product} product - The product to get the quantity of.
+   * @returns {string} The quantity of the product, formatted to 0 decimal places.
+   * @throws {Error} If the product is not found in the cart or its quantity is undefined.
+   */
   getProductQuantity(product: Product): string {
     const existingProduct = this._cartList.find(
       (el) => el.name === product.name
@@ -89,6 +130,11 @@ export class HelperCart {
     return existingProduct.quantity.toFixed(0)
   }
 
+  /**
+   * Returns the total quantity of all products in the cart.
+   * @returns {string} The total quantity of all products in the cart, formatted to 0 decimal places.
+   * @throws {Error} If any product in the cart has an undefined quantity.
+   */
   getTotalQuantity(): string {
     const totalQuantity = this._cartList.reduce((acc, curr) => {
       if (curr.quantity === undefined) {
@@ -100,6 +146,11 @@ export class HelperCart {
     return totalQuantity.toFixed(0)
   }
 
+  /**
+   * Returns the total price of all products in the cart.
+   * @returns {string} The total price of all products in the cart, formatted to 2 decimal places.
+   * @throws {Error} If any product in the cart has an undefined quantity.
+   */
   getTotalPrice(): string {
     const totalPrice = this._cartList.reduce((acc, curr) => {
       if (curr.quantity === undefined) {
