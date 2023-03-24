b514ad8e79f7b59356d214f0745c1f7cd3d0dd42
liviu-boros <boros.liviu@gmail.com>
2023-03-24 20:11:22 +0200

    Update Utilities
    - added jsdocs for each function

--- cypress/support/Utils.ts
+++ cypress/support/Utils.ts
@@ -1,16 +1,39 @@
 import { Data } from "@data"
 
+/**
+ * Sorts two strings alphabetically.
+ * Used for .sort()
+ *
+ * @param {string} a - The first string to compare.
+ * @param {string} b - The second string to compare.
+ * @returns {number} -1, 0, 1
+ */
 export const sortAlphabetically = (a: string, b: string): number => {
   if (a < b) return -1
   if (a > b) return 1
   return 0
 }
 
-export const selectRandom = <T>(products: T[]): T => {
-  const randomIndex = Math.floor(Math.random() * products.length)
-  return products[randomIndex]
+/**
+ * Selects a random item from an array.
+ *
+ * @template {T} - The type of items in the array.
+ * @param {T[]} list - The array of items to select from.
+ * @returns {T} A random item from the list.
+ */
+export const selectRandom = <T>(list: T[]): T => {
+  const randomIndex = Math.floor(Math.random() * list.length)
+  return list[randomIndex]
 }
 
+/**
+ * Paginates an array of items.
+ *
+ * @template {T} - The type of items in the array.
+ * @param {T[]} products - The array of items to paginate.
+ * @param {number} pageIndex - The page index to retrieve.
+ * @returns {T[]} The items on the specified page.
+ */
 export const paginateProducts = <T>(products: T[], pageIndex: number): T[] => {
   const itemsPerPage = Data.productsPerPage
   const start = (pageIndex - 1) * itemsPerPage
@@ -18,6 +41,14 @@ export const paginateProducts = <T>(products: T[], pageIndex: number): T[] => {
   return products.slice(start, end)
 }
 
+/**
+ * Filters an array of products by category.
+ *
+ * @template {T extends { category: { name: string } }} - The type of products in the array.
+ * @param {T[]} products - The array of products to filter.
+ * @param {string} filterBy - The name of the category to filter by.
+ * @returns {T[]} The products that belong to the specified category.
+ */
 export const filterProductsByCategory = <
   T extends { category: { name: string } }
 >(
